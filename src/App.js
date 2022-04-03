2b8d1419e9418b98544d862d8c7dd3dbcec20989
parvaz1212 <102854030+parvaz1212@users.noreply.github.com>
2022-04-03 02:44:46 -0700

    added app.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,170 @@
-import logo from './logo.svg';
-import './App.css';
+import { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class app extends Component {
+    render() {
+        return (
+            <>
+            <div>
+            <form class="example" action="/action_page.php" style={{width:'300px',height:'30px',textAlign:'center',marginLeft:'480px',marginTop:'0px'}}>
+  <input type="text" placeholder="Search.." style={{height:'25px',borderRadius:'5px'}}/>
+  <button type="submit" style={{height:'30px',backgroundColor:'lightblue',color:'white',borderRadius:'5PX',}}>search</button>
+</form>
+  
+                <ul style={{listStyleType:'none',margin:'35px'}}>
+                  <li style={{border:'1px lightgray solid', width:'350px', height:'420px',boxShadow:'10px 10px rgb(240, 239, 239)',display:'inline-block',margin:'20px' }}>
+                        <img 
+                        src='asbb.png'
+                        style={{width: '350px',height: '250px',borderRadius: '2px'}}
+                        ></img>
+                        <h1 style={{color: 'rgb(87, 87, 255)',paddingLeft:'30px', FontSize: '25px', PaddingLeft: '30px', PaddingTop: '10px'}}>the hours</h1>
+                        <ul style={{listStyleType:'none'}}>
+                        <li>
+                                               <strong>views:</strong>
+                                                4000 
+                                            </li>
+                                            <li>
+                                               <strong>downloads:</strong>
+                                               300
+                                            </li>
+                                            <li>
+                                               <strong>likes:</strong>
+                                               200
+                                            </li>
+                        </ul>
+                    </li>
+                    <li style={{border:'1px lightgray solid', width:'350px', height:'420px',boxShadow:'10px 10px rgb(240, 239, 239)',display:'inline-block',margin:'20px'}}>
+                        <img 
+                        src='fish.png'
+                        style={{width: '350px',height: '250px',borderRadius: '2px'}}
+                        ></img>
+                        <h1 style={{color: 'rgb(87, 87, 255)',paddingLeft:'30px', FontSize: '25px', PaddingLeft: '30px', PaddingTop: '10px'}}>the fish</h1>
+                        <ul style={{listStyleType:'none'}}>
+                        <li>
+                                               <strong>views:</strong>
+                                                4000 
+                                            </li>
+                                            <li>
+                                               <strong>downloads:</strong>
+                                               300
+                                            </li>
+                                            <li>
+                                               <strong>likes:</strong>
+                                               200
+                                            </li>
+                        </ul>
+                    </li>
+                    <li style={{border:'1px lightgray solid', width:'350px', height:'420px',boxShadow:'10px 10px rgb(240, 239, 239)',display:'inline-block' , margin:'20px'}}>
+                        <img 
+                        src='sa.png'
+                        style={{width: '350px',height: '250px',borderRadius: '2px'}}
+                        ></img>
+                        <h1 style={{color: 'rgb(87, 87, 255)',paddingLeft:'30px', FontSize: '25px', PaddingLeft: '50px', PaddingTop: '30px',PaddingLeft:'40PX'}}>the dog</h1>
+                        <ul style={{listStyleType:'none'}}>
+                        <li>
+                                               <strong>views:</strong>
+                                                4000 
+                                            </li>
+                                            <li>
+                                               <strong>downloads:</strong>
+                                               300
+                                            </li>
+                                            <li>
+                                               <strong>likes:</strong>
+                                               200
+                                            </li>
+                        </ul>
+                    </li>
+                    <li style={{border:'1px lightgray solid', width:'350px', height:'420px',boxShadow:'10px 10px rgb(240, 239, 239)' ,display:'inline-block',margin:'20px'}}>
+                        <img 
+                        src='ca.png'
+                        style={{width: '350px',height: '250px',borderRadius: '2px'}}
+                        ></img>
+                        <h1 style={{color: 'rgb(87, 87, 255)',paddingLeft:'30px', FontSize: '25px', PaddingLeft: '30px', PaddingTop: '10px'}}>the cat</h1>
+                        <ul style={{listStyleType:'none'}}>
+                        <li>
+                                               <strong>views:</strong>
+                                                4000 
+                                            </li>
+                                            <li>
+                                               <strong>downloads:</strong>
+                                               300
+                                            </li>
+                                            <li>
+                                               <strong>likes:</strong>
+                                               200
+                                            </li>
+                        </ul>
+                    </li>
+                    <li style={{border:'1px lightgray solid', width:'350px', height:'420px',boxShadow:'10px 10px rgb(240, 239, 239)' ,display:'inline-block',margin:'20px' }}>
+                        <img 
+                        src='fish1.png'
+                        style={{width: '350px',height: '250px',borderRadius: '2px'}}
+                        ></img>
+                        <h1 style={{color: 'rgb(87, 87, 255)',paddingLeft:'30px', FontSize: '25px', PaddingLeft: '30px', PaddingTop: '10px'}}>the fault</h1>
+                        <ul style={{listStyleType:'none'}}>
+                        <li>
+                                               <strong>views:</strong>
+                                                4000 
+                                            </li>
+                                            <li>
+                                               <strong>downloads:</strong>
+                                               300
+                                            </li>
+                                            <li>
+                                               <strong>likes:</strong>
+                                               200
+                                            </li>
+                        </ul>
+                    </li>
+                    <li style={{border:'1px lightgray solid', width:'350px', height:'420px',boxShadow:'10px 10px rgb(240, 239, 239)',display:'inline-block',margin:'20px' }}>
+                        <img 
+                        src='pa.png'
+                        style={{width: '350px',height: '250px',borderRadius: '2px'}}
+                        ></img>
+                        <h1 style={{color: 'rgb(87, 87, 255)',paddingLeft:'30px', FontSize: '25px', PaddingLeft: '30px', PaddingTop: '10px'}}>the leopard</h1>
+                        <ul style={{listStyleType:'none'}}>
+                        <li>
+                                               <strong>views:</strong>
+                                                4000 
+                                            </li>
+                                            <li>
+                                               <strong>downloads:</strong>
+                                               300
+                                            </li>
+                                            <li>
+                                               <strong>likes:</strong>
+                                               200
+                                            </li>
+                        </ul>
+                    </li>
+                    <li style={{Display: 'inline-block',listStyleType:'none', Padding:'30PX'}}>
+                    <div style={{border:'1px lightgray solid', width:'350px', height:'420px',boxShadow:'10px 10px rgb(240, 239, 239)',display:'inline-block',margin:'20px'}}>
+                        <img 
+                        src='sh.png'
+                        style={{width: '350px',height: '250px',borderRadius: '2px'}}
+                        ></img>
+                        <h1 style={{color: 'rgb(87, 87, 255)',paddingLeft:'30px', FontSize: '25px', PaddingLeft: '30px', PaddingTop: '10px'}}>the lion</h1>
+                        <ul style={{listStyleType:'none'}}>
+                        <li>
+                                               <strong>views:</strong>
+                                                4000 
+                                            </li>
+                                            <li>
+                                               <strong>downloads:</strong>
+                                               300
+                                            </li>
+                                            <li>
+                                               <strong>likes:</strong>
+                                               200
+                                            </li>
+                        </ul>
+                      </div>
+                    </li>
+                </ul>
+            </div>
+            </>
+        )
+    }
 }
 
-export default App;
+export default app;
